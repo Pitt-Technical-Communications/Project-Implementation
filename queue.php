45847cb3d4aecfb59d32f567ec0624210377e956
mac479 <47116135+mac479@users.noreply.github.com>
2020-10-29 17:58:32 -0400

    added text and changed background

--- queue.php
+++ queue.php
@@ -1,8 +1,22 @@
 <?php
-    loadFile();
+
+    $fText= "";
+    $numInQ = 0;
+    $curWait = 0;
 
     //.= append string
     function loadFile(){
+        $queueFile= fopen("queue.txt", "r") or die("Unable to open file!");
+        $curTime= ((int)(date("H"))*60*60)+((int)(date("i"))*60)+((int)(date("s")));
+        while(!feof){
+            $cName=fgets();
+            $cTime=(int)(fgets());
+            if(cTime>curTime){
+                fText.=(cName . "\n" . cTime . "\n");
+                $numInQ++;
+                $curWait+=$cTime-$curTime;
+            }
+        }
 
     }
 
@@ -12,11 +26,15 @@
     function submitClick(){
         $queueFile= fopen("queue.txt", "r+") or die("Unable to open file!");
         $nextName = $_POST["nameSub"];
-        $nextSize = $_POST["numSub"];
-        while($nextName ){}
+        $nextSize = (int)($_POST["numSub"]);
+
+        //Should record time in seconds
+        $curTime= ((int)(date("H"))*60*60)+((int)(date("i"))*60)+((int)(date("s")));
+
+        while(!feof($queueFile)){}
         //I've never worked in php before I'm gonna need to get creative.
-        fwrite($queueFile, $nextName);
-        fwrite($queueFile, $nextName);
+        fwrite($queueFile, $nextName . "\n");
+        fwrite($queueFile, $nextSize . "\n");
         
         fclose($queueFile);
     }
